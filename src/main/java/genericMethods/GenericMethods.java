68e23a1a084d8f56baf12c017b8aed50151af428
sudheer <44312307+sudheer9651@users.noreply.github.com>
2023-12-08 14:55:21 +0530

    Update GenericMethods.java

--- src/main/java/genericMethods/GenericMethods.java
+++ src/main/java/genericMethods/GenericMethods.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 public class GenericMethods 
 {
 
