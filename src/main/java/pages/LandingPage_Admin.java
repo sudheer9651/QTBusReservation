6d4ea260e2f8a7bd00644dc9f83de2ec4c605b2e
SudheerIJ <asudheer.info@gmail.com>
2023-12-13 12:49:35 +0530

    added 2classes/pages

--- src/main/java/pages/LandingPage_Admin.java
+++ src/main/java/pages/LandingPage_Admin.java
@@ -1,13 +1,60 @@
 package pages;
 
 
+import java.io.FileInputStream;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import base.LaunchBrowser_Adminurl;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LandingPage_Admin extends LaunchBrowser_Adminurl
 
 {
+		public WebDriver LaunchAdminurl() throws Exception
+			{
+				Properties obj = new Properties();
+				FileInputStream fis = new FileInputStream(".\\src\\main\\resources\\ResourcesSpreadsheets\\config.properties");
+				obj.load(fis);
+				String BrowserName = obj.getProperty("browser");
+				String URL = obj.getProperty("url");
+				
+				if(BrowserName.equalsIgnoreCase("chrome"))
+				{
+					WebDriverManager.chromedriver().setup();
+					driver=new ChromeDriver();
+					
+				}
+				else if (BrowserName.equalsIgnoreCase("Edge"))
+				{
+					WebDriverManager.edgedriver();
+					driver=new EdgeDriver();					
+				}
+				else if (BrowserName.equalsIgnoreCase("Firefox"))
+				{
+					 WebDriverManager.firefoxdriver();
+					 driver=new FirefoxDriver();
+				}
+				driver.get(URL);
+				driver.manage().window().maximize();
+				
+				return driver; 		
+			}
 
-	
-	
-	
+		
+
+public static void main(String[] args) 
+	{
+		LandingPage_Admin admurlobj = new LandingPage_Admin();
+		try {
+				admurlobj.LaunchAdminurl();
+		    } 
+		catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+	}
+
 }
--- src/main/java/pages/LandingPage_User.java
+++ src/main/java/pages/LandingPage_User.java
@@ -1,13 +1,63 @@
 package pages;
 
 
+import java.io.FileInputStream;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import base.LaunchBrowser_Userurl;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LandingPage_User extends LaunchBrowser_Userurl
 
 {
 
-	
+	public WebDriver LaunchUserurl() throws Exception
+	{
+		Properties obj = new Properties();
+		FileInputStream fis = new FileInputStream(".\\src\\main\\resources\\ResourcesSpreadsheets\\config.properties");
+		obj.load(fis);
+		String BrowserName = obj.getProperty("browser");
+		String URL = obj.getProperty("User_url");
+		
+		if(BrowserName.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+			
+		}
+		else if (BrowserName.equalsIgnoreCase("Edge"))
+		{
+			WebDriverManager.edgedriver();
+			driver=new EdgeDriver();					
+		}
+		else if (BrowserName.equalsIgnoreCase("Firefox"))
+		{
+			 WebDriverManager.firefoxdriver();
+			 driver=new FirefoxDriver();
+		}
+		driver.get(URL);
+		driver.manage().window().maximize();
+		
+		return driver; 		
+	}
+
+
+
+public static void main(String[] args) 
+{
+	LandingPage_User admurlobj = new LandingPage_User();
+try {
+		admurlobj.LaunchUserurl();
+    } 
+catch (Exception e) 
+	{
+		e.printStackTrace();
+	}
+}
+
 	
 	
 }
