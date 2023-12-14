f046ac3b294d4f0d3bd0e2e35a71090ad71d17d9
VINAY <VINAY@DESKTOP-KN5QNI8>
2023-12-14 10:30:48 +0530

    adding vinay code

---
+++ src/test/java/Execution/vinay_bus_management1.java
@@ -0,0 +1,17 @@
+package Execution;
+
+import org.testng.annotations.Test;
+
+import Pages.Vinay_Bus_Management1;
+
+public class vinay_bus_management1 extends Vinay_Bus_Management1 {
+	
+	@Test
+	public void login() throws Exception {
+		
+		signIn();
+	}
+	
+	
+}
+	
